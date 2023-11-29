caa00859b96378c01d6e3485a4c43ea021d3ce1e
FatimaMadan <202001312@student.polytechnic.bh>
2023-11-29 16:35:59 +0300

    bot updated

--- botBackend.php
+++ botBackend.php
@@ -4,7 +4,6 @@ include 'debugging.php';
 
 // Include the necessary files and setup database connection
 
-
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
 
@@ -12,54 +11,46 @@ if (isset($_GET['action'])) {
         // Handle show FAQ action
         $result = FaqBank::getAllFaqs();
         if (!empty($result)) {
-             echo '<div class="bot-message">
-                 Please choose one of the courses below:
-                 <div class="option-buttons">';
-        
-        
-                        for ($i = 0; $i < count($result); $i++) {
-                           
-        echo '<button class="option-button" >'.$result[$i]->FQuestion.'</button>';
+            echo '<div class="bot-message">
+                Please choose one of the questions below:
+                <div class="option-buttons">';
+
+            for ($i = 0; $i < count($result); $i++) {
+                echo '<button class="option-button">' . $result[$i]->FQuestion . '</button>';
+            }
+
+            echo '</div> 
+            </div>';
+        } else {
+            echo '<button class="option-button">OPPPs</button>';
         }
-        echo '</div> 
-        </div>';
-                        } else {
-                        echo '<button class="option-button"> OPPPs </button>';
-                    }
-        
-        //getAllFaq
     } elseif ($action === 'majors') {
         // Handle show majors action
         $result = MajorBank::getAllMaj();
         if (!empty($result)) {
-             echo '<div class="bot-message">
-                 Please choose one of the courses below:
-                 <div class="option-buttons">';
-        
-        
-                        for ($i = 0; $i < count($result); $i++) {
-                           
-        echo '<button class="option-button" >'.$result[$i]->MajorName.'</button>';
-        }
-        echo '</div> 
-        </div>';
-                        } else {
-                        echo '<button class="option-button"> OPPPs </button>';
-                    }
-            
+            echo '<div class="bot-message">
+                Please choose one of the courses below:
+                <div class="option-buttons">';
 
-        
+            for ($i = 0; $i < count($result); $i++) {
+                echo '<button class="option-button" onclick="showCourses(\'show\')">' . $result[$i]->MajorName . '</button>';
+            }
+
+            echo '</div> 
+            </div>';
+            
+            echo '<div id="response-container"></div>';
+            
+        } else {
+            echo '<button class="option-button">OPPPs</button>';
+        }
     } else {
         // Invalid action
         echo 'Invalid action.';
     }
-    
 } else {
     // No action specified
     echo 'No action specified.';
 }
 
-
-
 ?>
-                        
